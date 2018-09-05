872e3ca7ae446e4c4e049ce038bc18a60e9a1b13
dejanstojanovic <d.stojanovic@hotmail.com>
2018-09-05 16:59:41 +0400

    Simple docker CI

---
+++ Sample.Service/ci.sh
@@ -0,0 +1,35 @@
+﻿#!/bin/bash
+
+#set github account
+githubaccount="dejanstojanovic"
+githubrepository="dotnetcore-windows-linux-service"
+gitbranch="single-library"
+
+#cleanup folders
+sudo rm $githubrepository -r
+
+
+#pull from github
+git clone  https://github.com/$githubaccount/$githubrepository.git
+cd ./dotnetcore-windows-linux-service/Sample.Service
+git checkout $gitbranch
+
+#set dockerhub account
+dockeraccount="dejanstojanovic"
+servicename="sampleservice"
+
+#get timestamp for the tag
+timestamp=$(date +%Y%m%d%H%M%S)
+
+#build image
+sudo docker build -t $servicename:$timestamp .
+
+#remove dangling images
+sudo docker system prune -f
+
+#push to dockerhub
+sudo docker login
+#sudo docker login -u username -p password
+sudo docker tag $servicename:$timestamp $dockeraccount/$servicename:$timestamp
+sudo docker push $dockeraccount/$servicename:$timestamp
+
